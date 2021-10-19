5cd5df8f76cbdca69ff79ff5c682d450bd7f56e9
Helen <yuhan.yangg@gmail.com>
2021-10-19 18:04:19 +0800

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import logo from "../images/logo.svg";
 const Header = () => {
-    return (
-        <>
-          <h1>Header</h1>  
-        </>
-    )
-}
+  return (
+    <>
+      <header className="header">
+          <div className="logo">
+              <img src={logo} alt="logo" />
+          </div>
+          <nav>
+              <ul>
+                  <li><button>About</button></li>
+                  <li><button>Careers</button></li>
+                  <li><button>Events</button></li>
+                  <li><button>Products</button></li>
+                  <li><button>Support</button></li>
+              </ul>
+          </nav>
+          <div className="menu-btn">
+              <div className="bar bar-1"></div>
+              <div className="bar bar-2"></div>
+              <div className="bar bar-3"></div>
+          </div>
+      </header>
+    </>
+  );
+};
 
-export default Header
+export default Header;
 

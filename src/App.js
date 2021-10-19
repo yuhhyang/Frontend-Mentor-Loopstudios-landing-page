0638167d3639b4772111cf548cba2125f6d2fddd
Helen <yuhan.yangg@gmail.com>
2021-10-19 18:47:03 +0800

    Interactive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
 import ShowImg from './components/ShowImg'
+import Interactive from './components/Interactive'
 const App = () => {
   return (
     <>
       <Header/>
       <ShowImg/>
+      <Interactive/>
     </>
   )
 }

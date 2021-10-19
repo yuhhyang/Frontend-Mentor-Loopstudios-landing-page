4dae2bacf872f44d4426988d3bee6977a69ba486
Helen <yuhan.yangg@gmail.com>
2021-10-19 19:38:49 +0800

    show creations-mobile

--- src/components/Creations.js
+++ src/components/Creations.js
@@ -1,11 +1,30 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import data from "../creations-mobile";
 const Creations = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const [cards, setCards] = useState(data);
+  return (
+    <section className="creations-mobile">
+        <h2>Our creations</h2>
+      <section>
+        {cards.map(card => {
+          const { id, image, title } = card;
+          return (
+            <article key={id}>
+              <div>
+                <img src={image} alt={image} />
+              </div>
+              <h3>{title}</h3>
+            </article>
+          );
+        })}
+      </section>
+      <div className="btn-container">
+          <button className="btn">
+              See All
+          </button>
+      </div>
+    </section>
+  );
+};
 
-export default Creations
+export default Creations;

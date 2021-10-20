0056bb9bc2674595a36f8d1ebe90f93dc831b3b8
Helen <yuhan.yangg@gmail.com>
2021-10-20 18:38:28 +0800

    mobile navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,27 +4,39 @@ const Header = () => {
   return (
     <>
       <header className="header">
-          <div className="logo">
-              <img src={logo} alt="logo" />
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <nav className="openNav">
+          <div className="navLogo">
+            <img src={logo} alt="logo" />
           </div>
-          <nav>
-              <ul>
-                  <li><button>About</button></li>
-                  <li><button>Careers</button></li>
-                  <li><button>Events</button></li>
-                  <li><button>Products</button></li>
-                  <li><button>Support</button></li>
-              </ul>
-          </nav>
-          <div className="menu-btn">
-              <div className="bar bar-1"></div>
-              <div className="bar bar-2"></div>
-              <div className="bar bar-3"></div>
-          </div>
+          <ul>
+            <li>
+              <button>About</button>
+            </li>
+            <li>
+              <button>Careers</button>
+            </li>
+            <li>
+              <button>Events</button>
+            </li>
+            <li>
+              <button>Products</button>
+            </li>
+            <li>
+              <button>Support</button>
+            </li>
+          </ul>
+        </nav>
+        <div className="menu-btn closeNav">
+          <div className="bar bar-1"></div>
+          <div className="bar bar-2"></div>
+          <div className="bar bar-3"></div>
+        </div>
       </header>
     </>
   );
 };
 
 export default Header;
-

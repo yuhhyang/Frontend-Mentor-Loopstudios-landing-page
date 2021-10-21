0068b42d59d65f2e7b1bc1762ca0e730ff1eb148
Helen <yuhan.yangg@gmail.com>
2021-10-22 03:19:45 +0800

    header scss

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
   };
   return (
     <>
-      <header className="header">
+      <header>
         <div className="logo">
           <img src={logo} alt="logo" />
         </div>
